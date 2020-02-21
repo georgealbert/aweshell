52c3de87300ff76671f7cd76d20dabe440f62294
Albert <georgealbert@qq.com>
2020-02-21 15:47:16 +0800

    disable extra prompt theme with git on Windows. Git is very slow on widnows.

--- aweshell.el
+++ aweshell.el
@@ -596,9 +596,9 @@ This advice can make `other-window' skip `aweshell' dedicated window."
 ;; eshell-prompt-extras
 ;; Display extra information and color for your eshell prompt.
 (require 'eshell-prompt-extras)
-(with-eval-after-load "esh-opt"
-  (setq eshell-highlight-prompt nil
-        eshell-prompt-function 'epe-theme-pipeline))
+;; (with-eval-after-load "esh-opt"
+;;   (setq eshell-highlight-prompt nil
+;;         eshell-prompt-function 'epe-theme-pipeline))
 
 ;; Validate command before post to eshell.
 (defun aweshell-validate-command ()
